51b4cf4264c3d7deacb16c3fd3bfc33caa12c163
Alin Koppandi <koppandi.alin@gmail.com>
2021-11-27 23:55:55 +0100

    Added dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3030;
 
 const server = app.listen(port, () =>
   console.log(`Express Server Listening on PORT ${port}`)
