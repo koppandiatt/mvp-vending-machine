95481423b953d9c54b6d1131dbeeac44d7b44a1b
Alin Koppandi <koppandi.alin@gmail.com>
2021-11-28 15:01:03 +0100

    added new product routes get, put

--- server/routes/product.js
+++ server/routes/product.js
@@ -6,6 +6,13 @@ import { validateProduct } from "./../models/product.js";
 const router = express.Router();
 
 router.get("/", product.list);
+router.get("/:id", validate.objectID, product.get);
 router.post("/", validate.body(validateProduct), product.create);
+router.put(
+  "/:id",
+  validate.objectID,
+  validate.body(validateProduct),
+  product.update
+);
 
 export default router;
