a22ad9cf52fe7a722cd3dce9d394d17126407aba
Alin Koppandi <koppandi.alin@gmail.com>
2021-11-28 01:11:49 +0100

    Refactored index,js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import winston from "winston";
+import initialize from "./startup/initialize.js";
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3030;
 
-const server = app.listen(port, () =>
-  console.log(`Express Server Listening on PORT ${port}`)
+initialize.logging();
+await initialize.database();
+
+const port = process.env.PORT || 8000;
+app.listen(port, () =>
+  winston.info(`Express Server Listening on PORT ${port}`)
 );
-
-export default server;
