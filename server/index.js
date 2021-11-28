237fc8787893168b09837217f3f6ebb17e932294
Alin Koppandi <koppandi.alin@gmail.com>
2021-11-28 12:24:06 +0100

    added routes to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
-import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
 import winston from "winston";
 import initializeServer from "./bootstrap/initialize.js";
+import routes from "./routes/index.js";
 
 dotenv.config();
+await initializeServer();
 
 const app = express();
 
-await initializeServer();
+app.use(express.json());
+app.use(cors());
+app.use(morgan("common"));
+app.use("/api", routes);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () =>
