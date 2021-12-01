fffe52b11fadafd5f75ffe51e8322155e88dcf13
Alin Koppandi <koppandi.alin@gmail.com>
2021-12-01 21:31:41 +0100

    Initial Client Commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/index.css";
+
+ReactDOM.render(
+  <React.StrictMode></React.StrictMode>,
+  document.getElementById("root")
+);
